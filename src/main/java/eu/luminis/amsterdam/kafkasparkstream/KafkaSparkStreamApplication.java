e0ddac566b0c2a805e79a56df3788ef0a7876df2
Sander Meinema <sander.meinema@luminis.eu>
2018-07-27 11:00:22 +0200

    Initial commit

---
+++ src/main/java/eu/luminis/amsterdam/kafkasparkstream/KafkaSparkStreamApplication.java
@@ -0,0 +1,12 @@
+package eu.luminis.amsterdam.kafkasparkstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSparkStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaSparkStreamApplication.class, args);
+    }
+}
